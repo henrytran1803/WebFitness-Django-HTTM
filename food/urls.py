7b8513c069ead692f72508f6f57924bd49f56cf6
Trần Việt Anh <tranvietanh1803@gmail.com>
2023-10-30 00:27:24 +0700

    food

--- food/urls.py
+++ food/urls.py
@@ -3,4 +3,7 @@ from . import views
 urlpatterns = [
 
     path('', views.food, name='food'),
+    path('track/', views.tracking, name='track'),
+    path('eat_track/<int:track_id>/', views.eat_track_detail, name='eat_track_detail'),
+    path('track/detail/', views.detail, name='detail'),
 ]
